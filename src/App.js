76b23a0427cdc62d8f763fa76718b06e905a7e29
MohammadKaifSaiyad <saiyedkgn6@gmail.com>
2024-03-23 15:38:27 +0530

    register/login completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import Register from './components/Register';
+import { Routes, Route } from 'react-router-dom';
+import Context from './Context';
 
 function App() {
   return (
-    <>
-    </>
+    <Context.Provider value={{}}>
+    <Routes>
+      <Route path='/register' element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+    </Routes>
+    </Context.Provider>
   );
 }
 
